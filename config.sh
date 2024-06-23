8d79eda3a0dfdd41c0dffa41863bbfa2535fb421
Rituraj Pandey <rp.weki.24@gmail.com>
2024-06-23 11:59:59 +0530

    *

--- config.sh
+++ config.sh
@@ -8,7 +8,48 @@ username="weki"
 ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
 hwclock --systohc
 
-pacman --noconfirm -S base-devel intel-ucode amd-ucode btrfs-progs grub efibootmgr linux-headers grub-btrfs networkmanager nano vim xorg-server git
+# Pacman Config
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+pacman -Syy
+
+# Mirror Selection
+pacman --noconfirm -S reflector
+reflector -c "India" -p https -a 7 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syy
+
+# Microcodes
+pacman --noconfirm -S intel-ucode amd-ucode
+
+# Firmware
+pacman --noconfirm -S sof-firmware linux-firmware-marvell
+
+# Server
+pacman --nocinfirm -S xorg-server xorg-xinit
+
+# File System
+pacman --noconfirm -S btrfs-progs e2fsprogs
+
+# Graphic Drivers
+pacman --noconfirm -S mesa vulkan-radeon vulkan-intel vulkan-nouveau xf86-video-amdgpu xf86-video-ati xf86-video-intel xf86-video-nouveau xf86-video-vmware intel-media-driver libva-intel-driver libva-mesa-driver
+
+# Bootloader
+pacman --noconfirm -S grub efibootmgr
+
+# Network
+pacman --noconfirm -S networkmanager network-manager-applet
+
+# Bluetooth
+pacman --noconfirm -S bluez bluez-utils blueman
+
+# Printer
+pacman --noconfirm -S cups
+
+# Audio
+pacman --noconfirm -S wireplumber pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire pavucontrol
+
+# Others
+pacman --noconfirm -S man nano
+
 
 # echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
@@ -23,22 +64,22 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
 
-echo "Set Root Password"
-passwd root
-
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Set Root Password"
+passwd root
 
 useradd -m -G wheel $username
 echo "Enter User Password: "
 passwd $username
 
-systemctl enable NetworkManager
-# systemctl enable bluetooth
-# systemctl enable cups.service
-# systemctl enable sshd
+# Start Services
+systemctl enable NetworkManager.service
+systemctl enable bluetooth.service
+systemctl enable cups.service
 systemctl enable reflector.timer
-# systemctl enable fstrim.timer
+systemctl enable fstrim.timer
 
 # sed -i 's/BINARIES=()/BINARIES=(btrfs setfont)/g' /etc/mkinitcpio.conf
 # mkinitcpio -p linux
@@ -52,3 +93,8 @@ systemctl enable reflector.timer
 
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 usermod -aG wheel $username
+
+##########################
+## OTHER CONFIGURATIONS ##
+##########################
+
