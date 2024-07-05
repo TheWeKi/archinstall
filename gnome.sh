c66a01e3ba1ea97625b30f5ba0e7157f7ecf5971
Rituraj Pandey <68937122+TheWeKi@users.noreply.github.com>
2024-07-05 19:14:24 +0530

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pacman --noconfirm -S xorg xorg-xinit fwupd gnome gnome-tweaks gnome-themes-extra power-profiles-daemon gnome-remote-desktop switcheroo-control cups usbutils
-
+pacman --noconfirm -S noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd vlc
 pacman --noconfirm -S mesa vulkan-intel intel-media-driver libva-utils libva-intel-driver
 
 sleep 1
